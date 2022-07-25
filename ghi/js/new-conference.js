7af9e0aeba43981bdcfea50c73a16e2b67a372fa
Shadan <shadanamini@gmail.com>
2022-07-25 11:14:47 -0700

    Created New Conference JavaScript

---
+++ ghi/js/new-conference.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = "http://localhost:8000/api/locations/";
+    const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            const selectTag = document.querySelector('#location')
+
+            for (const location of data.locations){
+                const option = document.createElement('option')
+                option.value = location.id
+                option.innerHTML = location.name
+                selectTag.appendChild(option)
+            }
+        }
+    const formTag = document.getElementById('create-conference-form');
+    formTag.addEventListener('submit', async function(event) {
+        event.preventDefault();
+        const formData = new FormData(formTag)
+        const json = JSON.stringify(Object.fromEntries(formData))
+        const conferenceUrl = "http://localhost:8000/api/conferences/";
+        const fetchConfig = {
+        method: "post",
+        body: json,
+        header: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const response = await fetch(conferenceUrl, fetchConfig);
+    if (response.ok){
+        formTag.reset();
+        const newConference = await response.json();
+        console.log(newConference)
+        }
+    });
+});
