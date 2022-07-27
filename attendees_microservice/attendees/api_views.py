dfc86083e115d2ecef6b5aa991c396c1f59c48d5
Shadan <shadanamini@gmail.com>
2022-07-27 11:41:25 -0700

    updated attendees view

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -56,7 +56,10 @@ def api_list_attendees(request, conference_vo_id=None):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        if conference_vo_id is not None:
+            attendees = Attendee.objects.filter(conference = conference_vo_id)
+        else:
+            attendees = Attendee.objects.all()
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
